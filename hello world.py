ab7af99ee113b55fdbc8be711076573d5295306b
TheDoco <dillonlewis@live.co.uk>
2016-09-29 15:25:01 +0100

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,17 @@
+### Letis have a small program here
+###Dillon Lewis 29/09/2016
+
+print("hello world")
+
+a = input("what is a?")
+b = input("what is b?")
+
+if(a==b):
+    print("they are the same size")
+else:
+    if(not a==b and a>b):
+        print("a is bigger")
+    else:
+        print("b is bigger")
+
+print("that was exciting...")
